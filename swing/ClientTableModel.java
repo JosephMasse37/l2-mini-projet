89ec42257bf61ffb1d9e8247d5af04bbbe49042b
Valentine Legent <valentine.legent33@gmail.com>
2026-01-04 02:20:23 +0100

    Implémentation getRowCount, getColumnCount, getValueAt dans ClientTableModel et création de JFrame.java

--- swing/ClientTableModel.java
+++ swing/ClientTableModel.java
@@ -1,25 +1,53 @@
 package swing;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.table.*;
 
+import metiers.Client;
+
 public class ClientTableModel extends AbstractTableModel {
+    private List<Client> clients;
+    private final String[] columnNames = {"ID", "Nom", "Prénom", "Âge", "Abonnement"};
+    
+    public ClientTableModel(List<Client> clients) {
+        this.clients = new ArrayList<>(clients);
+    }
 
     @Override
     public int getRowCount() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRowCount'");
+        return clients.size();
     }
 
     @Override
     public int getColumnCount() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getColumnCount'");
+        return columnNames.length;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getValueAt'");
+        //Get the client for the row
+        Client client = clients.get(rowIndex);
+
+        //Return the value for the column
+        switch (columnIndex) {
+            case 0:
+                return client.getIdClient();
+            case 1:
+                return client.getNom();
+            case 2:
+                return client.getPrenom();
+            case 3:
+                return client.getAge();
+            case 4:
+                if(client.getUnAbonnement() != null) {
+                    return client.getUnAbonnement().getFormule();
+                } else {
+                    return "Aucun";
+                }
+            default:
+                return null;
+        }
     }
     
 }
