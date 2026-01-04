0497a95304f5eeff37d5dbd957b1762c43186ff2
Valentine Legent <valentine.legent33@gmail.com>
2026-01-04 02:32:40 +0100

    Implémentation getRowCount, getColumnCount, getValueAt dans AbonnementTableModel et VehiculeTableModel

--- swing/AbonnementTableModel.java
+++ swing/AbonnementTableModel.java
@@ -1,25 +1,47 @@
 package swing;
 
 import javax.swing.table.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import metiers.Abonnement;
 
 public class AbonnementTableModel extends AbstractTableModel {
+    private List<Abonnement> abonnements;
+    private final String[] columnNames = {"Formule", "Prix", "Durée", "Nombre de Clients"};
+
+    public AbonnementTableModel(List<Abonnement> abonnements) {
+        this.abonnements = new ArrayList<>(abonnements);
+    }
 
     @Override
     public int getRowCount() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRowCount'");
+        return abonnements.size();
     }
 
     @Override
     public int getColumnCount() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getColumnCount'");
+        return columnNames.length;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getValueAt'");
+        //Get the Abonnement for the row
+        Abonnement abonnement = abonnements.get(rowIndex);
+
+        //Return the value for the column
+        switch (columnIndex) {
+            case 0 : 
+                return abonnement.getFormule();
+            case 1 :
+                return abonnement.getPrix();
+            case 2 :
+                return abonnement.getDuree();
+            case 3 :
+                return abonnement.getListeClients().size();
+            default :
+                return null;
+        }
     }
     
 }
